a83ab4551070b410dd3f18d1d2de646282585bc1
Gabriel Griffin <griffinsilva10@gmail.com>
2021-03-25 17:23:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hotel_premium
+ Hotel Sistema
---
+++ core/app/autoload.php
@@ -0,0 +1,16 @@
+<?php
+// autoload.php
+// 10 octubre del 2014
+// esta funcion elimina el hecho de estar agregando los modelos manualmente
+
+
+spl_autoload_register(function($modelname){
+	if(Model::exists($modelname)){
+		include Model::getFullPath($modelname);
+	} 
+
+})
+
+
+
+?>
